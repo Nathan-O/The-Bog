4b45178a279d85cc91816d4855ec3feb9ff19897
Nathan Ockerman <ockerman.ns@gmail.com>
2015-09-18 19:15:21 -0700

    first route written. Plugging along like no one's business.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-
+   RouteApp::Application.routes.draw do
+      root to: "creatures#index"
+      get "/creatures", to: "creatures#index"
+   end
 end
+
+
+# RouteApp::Application.routes.draw do
+#     root to: 'creautres#index'
+#     ## Also just to keep it RESTful
+#     get '/creatures', to: "creatures#index"
+# end
